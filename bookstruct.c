68ef0430631319bddb8f423981d2ee561a041e09
Mona Lisa <holmesmainsend@gmail.com>
2022-10-23 12:41:09 -0400

    added book1

--- bookstruct.c
+++ bookstruct.c
@@ -10,8 +10,11 @@ struct Book {
 };
 
 int main() {
-    struct Book Book1;
-    
+    struct Book book1;
+    book1.pageCount = 328;
+    book1.rating = 4.1;
+    strcpy(book1.author, "Hemingway");
+    strcpy(book1.title, "The Sun Also Rises");
 
     return 0;
 }
