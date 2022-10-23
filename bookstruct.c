f887c41d699a5bdbfaa3ff54aa350777f6db0528
Mona Lisa <holmesmainsend@gmail.com>
2022-10-23 12:37:09 -0400

    added initial struct details

--- bookstruct.c
+++ bookstruct.c
@@ -1,8 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main() {
+struct Book {
+    char title[50];
+    char author[50];
+    int publicationYear;
+    int pageCount;
+    double rating;
+};
 
+int main() {
+    struct Book Book1;
     
 
     return 0;
