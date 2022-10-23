347be088c509143dec3f17da9b137248c2fec07e
Mona Lisa <holmesmainsend@gmail.com>
2022-10-23 12:46:34 -0400

    tested printing struct values

--- bookstruct.c
+++ bookstruct.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 struct Book {
-    char title[50];
-    char author[50];
+    char titleNoSpaces[50];
+    char authorLastName[50];
     int publicationYear;
     int pageCount;
     double rating;
@@ -13,8 +14,10 @@ int main() {
     struct Book book1;
     book1.pageCount = 328;
     book1.rating = 4.1;
-    strcpy(book1.author, "Hemingway");
-    strcpy(book1.title, "The Sun Also Rises");
+    strcpy(book1.authorLastName, "Hemingway");
+    strcpy(book1.titleNoSpaces, "TheSunAlsoRises");
+
+    printf("%s\n", book1.titleNoSpaces);
 
     return 0;
 }
