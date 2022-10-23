21c3f615c2281665237c1a193c253f3e16ef2a37
Mona Lisa <holmesmainsend@gmail.com>
2022-10-23 12:50:04 -0400

    fixed book2 inputs

--- bookstruct.c
+++ bookstruct.c
@@ -18,10 +18,10 @@ int main() {
     strcpy(book1.titleNoSpaces, "TheSunAlsoRises");
 
     struct Book book2;
-    book1.pageCount = 204;
-    book1.rating = 3.6;
-    strcpy(book1.authorLastName, "Salinger");
-    strcpy(book1.titleNoSpaces, "TheCatcherInTheRye");
+    book2.pageCount = 204;
+    book2.rating = 3.6;
+    strcpy(book2.authorLastName, "Salinger");
+    strcpy(book2.titleNoSpaces, "TheCatcherInTheRye");
 
     printf("%s\n", book2.authorLastName);
 
